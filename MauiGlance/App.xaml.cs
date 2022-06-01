738f48dc1924fe4eef06bd87ae5b61dec2153a4e
Iron <iron.yehong@outlook.com>
2022-06-02 00:54:42 +0800

    Initialize project with .NET MAUI application.

---
+++ MauiGlance/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGlance;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiGlance/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiGlance;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
